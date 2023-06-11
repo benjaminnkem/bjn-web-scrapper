2efc4ab2b0d21409aa4b67328334e0424b627824
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-06-11 02:06:16 +0100

    Finished crawling code, still has little errors... Buh it works

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 const express = require("express");
 const cheerio = require("cheerio");
@@ -30,6 +31,7 @@ app.get("/scrape/:protocol/:domain/go", async (req, res) => {
   const mainUrl = `${protocol}://${domain}`;
 
   const allLinks = new Set();
+  const visitedLinks = new Set();
 
   async function sendRequests(url) {
     const request = await axios.get(url);
@@ -38,11 +40,32 @@ app.get("/scrape/:protocol/:domain/go", async (req, res) => {
     return htmlBody;
   }
 
+  function hasVisited(link) {
+    return visitedLinks.has(link);
+  }
+
   function cleanUrls(url, setToAddTo) {
-    if (url.startsWith(`${protocol}://${domain}`)) {
+    if (url.startsWith(`${protocol}://${domain}`) && !url.endsWith("/")) {
       setToAddTo.add(url);
+      visitedLinks.add(url);
     } else if (url.startsWith("/") && !url.endsWith("/")) {
       setToAddTo.add(`${protocol}://${domain}${url}`);
+      visitedLinks.add(`${protocol}://${domain}${url}`);
+    } else if (url.startsWith("/") && url.endsWith("/")) {
+      setToAddTo.add(`${protocol}://${domain}${url.substring(0, url.length - 1)}`);
+      visitedLinks.add(`${protocol}://${domain}${url.substring(0, url.length - 1)}`);
+    } else {
+      return;
+    }
+  }
+
+  function retrieveCleanUrl(url) {
+    if (url.startsWith(`${protocol}://${domain}`) && !url.endsWith("/")) {
+      return url;
+    } else if (url.startsWith("/") && !url.endsWith("/")) {
+      return `${protocol}://${domain}${url}`;
+    } else if (url.startsWith("/") && url.endsWith("/")) {
+      return `${protocol}://${domain}${url.substring(0, url.length - 1)}`;
     } else {
       return;
     }
@@ -55,11 +78,15 @@ app.get("/scrape/:protocol/:domain/go", async (req, res) => {
       const linkUrl = $(this).attr("href");
       cleanUrls(linkUrl, allLinks);
     });
+  } catch (error) {
+    console.log(error.message);
+  }
 
-    // Consecutive requests
-    let currentListIndex = 0;
-    let nextListIndex = 20;
-    let convertedList = Array.from(allLinks).slice(currentListIndex, nextListIndex);
+  try {
+    // // Consecutive requests
+    // let currentListIndex = 0;
+    // let nextListIndex = 20;
+    // let convertedList = Array.from(allLinks).slice(currentListIndex, nextListIndex);
 
     // async function crawlOtherLinks() {
     //   if (nextListIndex <= Array.from(allLinks).length) {
@@ -82,21 +109,32 @@ app.get("/scrape/:protocol/:domain/go", async (req, res) => {
     //     nextListIndex += remainingNumberOfLinks;
     //   }
     // }
-  } catch (error) {
-    console.log(error.message);
-  }
 
-  try {
-    const gottenLinks = new Set();
     async function crawlOtherLinks(links) {
-      for (const link of links) {
-        console.log(link);
-        const $ = cheerio.load(await sendRequests(link));
-        $("a").each(function () {
-          const linkUrl = $(this).attr("href");
-          cleanUrls(linkUrl, gottenLinks);
-        });
+      const gottenLinks = new Set();
+      // This just loops through the first links of the "allLinks" list
+      async function repeatCrawling(links) {
+        for (const link of links) {
+          const $ = cheerio.load(await sendRequests(link));
+          $("a").each(function () {
+            const linkUrl = $(this).attr("href");
+            if (!hasVisited(retrieveCleanUrl(linkUrl))) {
+              cleanUrls(linkUrl, gottenLinks);
+              cleanUrls(linkUrl, allLinks);
+            }
+          });
+        }
       }
+
+      // Initial crawl repeat results
+      await repeatCrawling(links);
+
+      while (gottenLinks.size <= 5000) {
+        // This just loops through consecutive links of the "gottenLinks" list
+        await repeatCrawling([...gottenLinks]);
+      }
+
+      console.log("Crawling completed!", visitedLinks.size, gottenLinks.size);
 
       for (const newLink of gottenLinks) {
         allLinks.add(newLink);
@@ -104,8 +142,29 @@ app.get("/scrape/:protocol/:domain/go", async (req, res) => {
     }
 
     await crawlOtherLinks([...allLinks]);
+
+    // Creating the XML document
+    const filePath = "./sitemap/sitemap.xml";
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+    for (const link of allLinks) {
+      xml += `<url>
+        <loc>${link}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <priority>1</priority>
+      </url>`;
+    }
+    xml += "</urlset>";
+
+    fs.writeFileSync(filePath, xml, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    console.log(xml);
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 
   res.status(200).json([...allLinks]);
